ad79d8334b5f23381d68d7f4e464baab69a6b1e2
João Simões <gravity00@users.noreply.github.com>
2023-03-16 12:26:51 +0000

    added tests for collections

---
+++ MicrosoftDependencyInjection/MicrosoftDependencyInjection/Collection/CollectionTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace MicrosoftDependencyInjection.Collection;
+
+public class CollectionTests
+{
+    [Fact]
+    public void ContainerInitialization()
+    {
+        using var provider = new ServiceCollection()
+            .AddSingleton<IService, ServiceA>()
+            .AddSingleton<IService, ServiceB>()
+            .BuildServiceProvider();
+
+        var services = provider.GetServices<IService>().ToArray();
+
+        Assert.Equal(2, services.Length);
+        Assert.Collection(services,
+            item => Assert.IsType<ServiceA>(item),
+            item => Assert.IsType<ServiceB>(item)
+        );
+    }
+
+    [Fact]
+    public void ManualInitialization()
+    {
+        var services = new IService[]
+        {
+            new ServiceA(),
+            new ServiceB()
+        };
+
+        Assert.Equal(2, services.Length);
+        Assert.Collection(services,
+            item => Assert.IsType<ServiceA>(item),
+            item => Assert.IsType<ServiceB>(item)
+        );
+    }
+}
+
+public interface IService
+{
+    Guid Id { get; }
+}
+
+public class ServiceA : IService
+{
+    public Guid Id { get; } = Guid.NewGuid();
+}
+
+public class ServiceB : IService
+{
+    public Guid Id { get; } = Guid.NewGuid();
+}
